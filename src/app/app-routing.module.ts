04897c4293d5c3b813cede93ba41c36ebb772aba
DESKTOP-QOQTLA1\earth <pattarapol42@gmail.com>
2022-11-22 22:48:47 +0700

    Add dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: "login", component: LoginComponent
-},
-{
-  path: "**", redirectTo: "login"
-}];
+const routes: Routes = [
+  {
+    path: "login", component: LoginComponent
+  },
+  {
+    path: "dashboard", component: DashboardComponent
+  },
+  {
+    path: "**", redirectTo: "login"
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
